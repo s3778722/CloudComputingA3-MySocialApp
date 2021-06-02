2121d23d086dd986fd53fb6f6811e20ea023e624
s3778722 <s3778722@student.rmit.edu.au>
2021-06-02 09:01:37 +0800

    Profile and partial homepage implemented

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session, g
 import requests
 import boto3
+import os
 from boto3.dynamodb.conditions import Key, Attr
+from werkzeug.utils import secure_filename
+from datetime import datetime
+dirname = os.path.dirname(__file__)
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
@@ -9,6 +13,7 @@ app.secret_key = 'mysecretkey'
 dynamodb = boto3.resource('dynamodb')
 dynamodb_client = boto3.client('dynamodb')
 s3 = boto3.client('s3')
+BUCKET_NAME = 'mysocialapp'
 users = dynamodb.Table('users')
 
 @app.before_request
@@ -20,17 +25,74 @@ def before_request():
         user_email = session['user_email']
         g.email = user_email
         
-        response = users_table.query(
+        response = users.query(
         KeyConditionExpression=Key('email').eq(g.email)
         )
-        g.username = response['Items'][0]['user_name']
+        g.username = response['Items'][0]['username']
 
 @app.route('/')
 def index():
     return render_template('index.html', is_index = True)
 
+@app.route('/home',methods=['GET','POST'])
+def home():
+    response = users.get_item(Key={'email': g.email})
+    user = response['Item']
+    if request.method == 'GET':
+        return render_template('home.html', user = user)
+    
+@app.route('/edit/profile',methods=['GET','POST'])
+def edit_profile():
+    if request.method == 'GET':
+        response = users.get_item(Key={'email': g.email})
+        user = response['Item']
+        return render_template('edit_profile.html', user = user)
+    elif request.method == 'POST':   
+        response = users.get_item(Key={'email': g.email})
+        user = response['Item']
+             
+        username = request.form.get('edit-username')
+        about = request.form.get('edit-aboutme')
+        age = request.form.get('edit-age')
+        location = request.form.get('edit-location')
+        img = request.files['edit-image']
+
+        if img:
+            image = 'https://mysocialapp.s3.us-east-2.amazonaws.com/' + g.email
+            filename = secure_filename(img.filename)
+            filename = os.path.join(dirname, 'static/img/'+ filename)
+            img.save(filename)
+            s3.upload_file(
+                Bucket = BUCKET_NAME,
+                Filename=filename,
+                Key = g.email
+            )
+        else:
+            if 'image_path' in user:
+                image = user['image_path']
+            else:
+                image = None
+
+        users.update_item(
+        Key={
+            'email': g.email
+        },
+        UpdateExpression="set username = :u, about = :a, age = :age, loc= :loc, image_path = :i",
+        ExpressionAttributeValues={
+            ":u" : username,
+            ":a" : about,
+            ":age" : age,
+            ":loc" : location,
+            ":i" : image,                  
+        }
+        )
+        return redirect(url_for('edit_profile'))
+
 @app.route('/signup',methods=['GET','POST'])
 def register():
+    date = datetime.now()
+    date = date.strftime('%Y-%m-%d')
+    
     if 'user_email' in session:
         return redirect(url_for('home'))
     
@@ -54,7 +116,8 @@ def register():
                 Item={
                     'email': email_signup,
                     'username': username_signup,
-                    'password' : password_signup
+                    'password' : password_signup,
+                    'date' : date
                 }
             )
             return redirect(url_for('login'))
@@ -81,14 +144,14 @@ def login():
             error = "Email or password is invalid"  
         else:
             session['user_email'] = email_login
-            return redirect(url_for('index'))
+            return redirect(url_for('home'))
     return render_template('login.html',error = error)
 
 @app.route('/logout')
 def logout():
    session.pop('user_email', None)
    return redirect(url_for('login'))
-
+        
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
  
